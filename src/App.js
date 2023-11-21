659265f1b631ee34e0c05323698fa529872e0edd
kgpksh <tkdgns543@gmail.com>
2023-11-21 22:52:16 +0900

    feat: 프로젝트 초기 세팅, 서버와 테스트 통신 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [testMsg, setTestMsg] = useState('')
+  useEffect(() => {
+    fetch('/member/test')
+    .then(res => res.text())
+    .then(msg => setTestMsg(msg))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{testMsg}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
