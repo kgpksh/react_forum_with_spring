9044dcd7d59357b2497dd0395751c00d6efe3b61
kgpksh <tkdgns543@gmail.com>
2023-11-24 02:32:03 +0900

    feat: 로그인 하지 않은 경우에만 회원가입, 로그인 버튼 보이게 수정

--- src/header/UserLinks.js
+++ src/header/UserLinks.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function UserLinks() {
+    const [isLogin, setLogin] = useState(localStorage.getItem('isLogin'))
     return (
         <div className="User">
             <div><h2><Link to={'/'}>To home</Link></h2></div>
-            <div><Link to={'/member/signup'}>회원가입</Link></div>
-            <div>로그인</div>
+            <div><Link to={'/member/signup'}>{isLogin &&'회원가입'}</Link></div>
+            <div>{isLogin &&'로그인'}</div>
         </div>
     )
 }
