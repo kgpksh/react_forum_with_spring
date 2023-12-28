5fe5d6735f127f6984d97336e31a6fa249ebdcc4
kgpksh <tkdgns543@gmail.com>
2023-12-29 01:01:47 +0900

    jwt 만료 확인 기능에서 jwt 내용 가져오는 기능 분리

--- src/utils/checkJwtExpired.js
+++ src/utils/checkJwtExpired.js
@@ -1,14 +1,12 @@
+import jwtContentGetter from "./jwtContentGetter";
+
 export default function checkJwtExpired(){
-    const token = localStorage.getItem('jwtToken')
-    if(token == null) {
+    const jsonPayload = jwtContentGetter()
+    if(jsonPayload == null) {
         return true
     }
-    const base64Payload = token.split('.')[1];
-    const base64 = base64Payload.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    
     const now = new Date().getTime()
-    const jwtExpiringTime = Number(JSON.parse(jsonPayload).exp) * 1000
+    const jwtExpiringTime = Number(jsonPayload.exp) * 1000
     return jwtExpiringTime <= now
 };
---
+++ src/utils/jwtContentGetter.js
@@ -0,0 +1,13 @@
+export default function jwtContentGetter() {
+    const token = localStorage.getItem('jwtToken')
+    if(token == null) {
+        return null
+    }
+    const base64Payload = token.split('.')[1];
+    const base64 = base64Payload.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload)
+}
